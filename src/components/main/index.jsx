9d0cd16418b85cb24878918c933594e85aaffa70
Moiseis Cruz <moizzito@gmail.com>
2023-12-29 19:53:39 -0300

    Criei todo os eventos dos botões.

--- src/components/main/index.jsx
+++ src/components/main/index.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react"
 
-async function getDatosZelda(){
-    const response = await fetch('https://zelda.fanapis.com/api/games')
+async function getDatosZelda(pageIndex){
+    const response = await fetch(`https://zelda.fanapis.com/api/games?limit=6&page=${pageIndex}`)
     const datos = await response.json()
     return datos.data
 }
@@ -10,42 +10,44 @@ export const Main = () => {
 
     const [ gameZelda, setGameZelda ] = useState({game:[]})
 
-    const GamesList = (props) => {
-        return(
-            <ul>
-                {
-                    props.games.map((item, index) => {
-                        return(
-                            <li key={index}>
-                                <div style={{border: '5px solid red'}}>
-                                    <h1>{item.name}</h1>
-                                    <h2><b>Developer:</b> {item.developer}</h2>
-                                    <h2><b>Publiser: </b>{item.publisher}</h2>
-                                    <h4><b>Released Date: </b>{item.released_date}</h4>
-                                    <p><b>Description: </b>{item.description}</p>
-                                </div>
-                            </li>
-                        )
-                    })
-                }
-            </ul>
-        )
-    }
+    const [ pageIndex, setPageIndex ] = useState(0)
 
     useEffect(() => {
         const fetchData = async () => {
-            const zeldaDatos = await getDatosZelda()
+            const zeldaDatos = await getDatosZelda(pageIndex)
 
             setGameZelda({game: zeldaDatos})
         }
         fetchData()
-    },[])
+    },[pageIndex])
 
     return(
         <section>
             {gameZelda.game.length > 0 ? <GamesList games={gameZelda.game} /> : "❌❌ Não há nada sobre a franquia ❌❌"}
-            <button>Prev Page</button>
-            <button>Next Page</button>
+            <button onClick={() => setPageIndex((state) => state - 1)} disabled={pageIndex === 0}>Prev Page</button>
+            <button onClick={() => setPageIndex((state) => state + 1)} disabled={pageIndex >= gameZelda.game.length} >Next Page</button>
         </section>
+    )
+}
+
+const GamesList = (props) => {
+    return(
+        <ul>
+            {
+                props.games.map((item, index) => {
+                    return(
+                        <li key={index}>
+                            <div style={{border: '5px solid red'}}>
+                                <h1>{item.name}</h1>
+                                <h2><b>Developer:</b> {item.developer}</h2>
+                                <h2><b>Publiser: </b>{item.publisher}</h2>
+                                <h4><b>Released Date: </b>{item.released_date}</h4>
+                                <p><b>Description: </b>{item.description}</p>
+                            </div>
+                        </li>
+                    )
+                })
+            }
+        </ul>
     )
 }
