5c6af7c96c7625fda8be952c8f5e1f2e87453e6c
Moiseis Cruz <moizzito@gmail.com>
2024-01-01 18:42:18 -0300

    I did the global styling.

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,24 @@
 import './App.css'
 import { Main } from './components/main'
+import { createGlobalStyle } from 'styled-components'
 
 function App() {
   return (
     <>
+      <GlobalStyle />
       <Main/>
     </>
   )
 }
 
 export default App
+
+
+const GlobalStyle = createGlobalStyle`
+  ul {
+    list-style: none;
+    display: flex;
+    flex-direction: column;
+    gap: 15px;
+  }
+`
