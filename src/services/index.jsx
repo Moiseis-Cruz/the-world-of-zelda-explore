f2debfa3f25840dcbe44152204522e732e45e97b
Moiseis Cruz <moizzito@gmail.com>
2024-01-10 21:15:43 -0300

    I added a condition for the api fetch.

--- src/services/index.jsx
+++ src/services/index.jsx
@@ -1,5 +1,10 @@
 export async function getDatosZelda(pageIndex){
     const response = await fetch(`https://zelda.fanapis.com/api/games?limit=6&page=${pageIndex}`);
+
+    if(!response.ok){
+        throw new Error(`API Zeld returned status code ${response.status}`)
+    }
+
     const datos = await response.json()
     return datos.data
 }
