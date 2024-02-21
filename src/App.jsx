5710fae5797d4c4c8ddf6a88bcec1bf81ae45c63
Moiseis Cruz <moizzito@gmail.com>
2024-02-21 18:18:51 -0300

    I removed GlobalStyles.

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
 import './App.css'
 import { HeaderLogo } from './components/header'
 import { Main } from './components/main'
-import { GlobalStyle } from './style/GlobalStyles'
 
 function App() {
   return (
     <>
-      <GlobalStyle />
       <HeaderLogo/>
       <Main/>
     </>
