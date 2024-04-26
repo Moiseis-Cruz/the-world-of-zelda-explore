a67166e1fa0a40c78be830840163f4f81ec9723a
Moiseis Cruz <moizzito@gmail.com>
2024-04-26 12:34:54 -0300

    I added button styling to Styles.js

--- src/style/Styles.js
+++ src/style/Styles.js
@@ -1,5 +1,30 @@
 import styled from "styled-components";
 
+export const DivBtns = styled.div `
+    margin-top: 50px;
+    display: flex;
+    gap: 25px;
+    align-items: center;
+    justify-content: center;
+`
+
+export const BtnPage = styled.button`
+    background-color: #c2bd85;
+    color: #edecec;
+    &:hover{
+        box-shadow: 0 0 15px 5px #686868;
+    }
+    &:active{
+        transform: scale(1.1);
+    }
+    &:disabled{
+        box-shadow: none;
+        transform: scale(1);
+        cursor: no-drop;
+        opacity: 0.5;
+    }
+`
+
 export const ListGames = styled.ul`
     display: flex;
     flex-wrap: wrap;
