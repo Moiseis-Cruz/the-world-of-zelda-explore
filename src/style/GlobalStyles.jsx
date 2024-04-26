305e93feffd82dc8286e5a7debabe0a10995c23d
Moiseis Cruz <moizzito@gmail.com>
2024-04-26 12:29:06 -0300

    I added the scrollbars styling to Global Styles.

--- src/style/GlobalStyles.jsx
+++ src/style/GlobalStyles.jsx
@@ -7,8 +7,25 @@ export const GlobalStyle = createGlobalStyle`
     box-sizing: border-box;
 }
 
+::-webkit-scrollbar {
+    width: 6px;
+}
+
+::-webkit-scrollbar-track {
+    background-color: #686868;
+}
+
+::-webkit-scrollbar-thumb {
+    background-color: #06c7a0;
+    border-radius: 15px;
+}
+
 body {
     font-family: 'Genos', sans-serif;
+    max-width: 1440px;
+    margin: 0 auto;
+    padding: 2rem;
+    text-align: center;
 }
 
 ul {
