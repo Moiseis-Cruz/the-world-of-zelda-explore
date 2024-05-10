ee2c330653cff3053e0ad6bd134e774710c83984
Moiseis Cruz <moizzito@gmail.com>
2024-05-10 20:41:24 -0300

    I added the Global stylings of :root, body and button.

--- src/style/GlobalStyles.jsx
+++ src/style/GlobalStyles.jsx
@@ -1,6 +1,21 @@
 import { createGlobalStyle } from 'styled-components';
 
 export const GlobalStyle = createGlobalStyle`
+:root {
+    font-family: Inter, system-ui, Avenir, Helvetica, Arial, sans-serif;
+    line-height: 1.5;
+    font-weight: 400;
+
+    color-scheme: light dark;
+    color: rgba(255, 255, 255, 0.87);
+    background-color: #242424;
+
+    font-synthesis: none;
+    text-rendering: optimizeLegibility;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+}
+
 * {
     margin: 0;
     padding: 0;
@@ -22,10 +37,33 @@ export const GlobalStyle = createGlobalStyle`
 
 body {
     font-family: 'Genos', sans-serif;
+    display: flex;
+    place-items: center;
     max-width: 1440px;
+    min-width: 320px;
+    min-height: 100vh;
     margin: 0 auto;
     padding: 2rem;
     text-align: center;
+}
+
+button {
+    border-radius: 8px;
+    border: 1px solid transparent;
+    padding: 0.6em 1.2em;
+    font-size: 1em;
+    font-weight: 500;
+    font-family: inherit;
+    background-color: #1a1a1a;
+    cursor: pointer;
+    transition: border-color 0.25s;
+}
+button:hover {
+    border-color: #646cff;
+}
+button:focus,
+button:focus-visible {
+    outline: 4px auto -webkit-focus-ring-color;
 }
 
 ul {
